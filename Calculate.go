20287c0ba9ee8d12f0070ca135e1b33b0cc2e604
NathanVrieland <78039929+NathanVrieland@users.noreply.github.com>
2021-10-27 11:13:26 -0400

    improved readability

--- Calculate.go
+++ Calculate.go
@@ -15,16 +15,18 @@ func z(mean float64, stdev float64, value float64) float64 {
 }
 
 func main() {
-	var num_rectangles = 100000000
+	var NUM_RECTANGLES = 100000000
 	var mean, stdev, x0, xn float64
 	fmt.Println("Please enter:(mean stdev x0 xn)")
-	fmt.Scan(&mean)
-	fmt.Scan(&stdev)
-	fmt.Scan(&x0)
-	fmt.Scan(&xn)
+	{
+		fmt.Scan(&mean)
+		fmt.Scan(&stdev)
+		fmt.Scan(&x0)
+		fmt.Scan(&xn)
+	} // inputs
 	Start :=time.Now()
 
-	fmt.Printf("%.16f\n", goGetSum(z(mean, stdev, x0),z(mean, stdev, xn), num_rectangles, runtime.NumCPU()))
+	fmt.Printf("%.16f\n", goGetSum(z(mean, stdev, x0), z(mean, stdev, xn), NUM_RECTANGLES, runtime.NumCPU()))
 
 	duration := time.Since(Start)
 	fmt.Println("Compute Time: " + fmt.Sprint(duration))
